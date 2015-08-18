cf147b4662deeda38c129925db8ae63f0e05306a
unknown <gjdipietro@gmail.com>
2015-08-18 10:19:31 -0400

    added ignore to bower.json

---
+++ possessive.js
@@ -0,0 +1,9 @@
+angular.module('filter.possessive', [])
+    .filter('possessive', function () {
+        return function (properNoun) {
+            if (properNoun.charAt(properNoun.length-1) == "s" || properNoun.charAt(properNoun.length-1) == "S") {
+                return properNoun + "'";
+            }
+            else return properNoun + "'s";
+        }
+    });
