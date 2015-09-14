7462476215d1ac48b3909f24692c64ed940f0de8
unknown <gjdipietro@gmail.com>
2015-09-14 11:15:56 -0400

    updating bower release

--- possessive.js
+++ possessive.js
@@ -2,12 +2,12 @@
 angular.module('filter.possessive', [])
     .filter('possessive', function () {
         return function (properNoun) {
-            if (properNoun == null) {
+            if (properNoun === null) {
                 return properNoun;
             }
             if (properNoun.charAt(properNoun.length-1) == "s" || properNoun.charAt(properNoun.length-1) == "S") {
                 return properNoun + "'";
             }
             else return properNoun + "'s";
-        }
+        };
     });
