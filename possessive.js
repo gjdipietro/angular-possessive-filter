874db684767e14f9feeedd453d443994690c35ff
unknown <gjdipietro@gmail.com>
2015-09-15 11:31:18 -0400

    removed strict equality

--- possessive.js
+++ possessive.js
@@ -2,7 +2,7 @@
 angular.module('filter.possessive', [])
     .filter('possessive', function () {
         return function (properNoun) {
-            if (properNoun === null) {
+            if (properNoun == null) {
                 return properNoun;
             }
             if (properNoun.charAt(properNoun.length-1) == "s" || properNoun.charAt(properNoun.length-1) == "S") {
